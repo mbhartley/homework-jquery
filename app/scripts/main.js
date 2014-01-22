91bc17a6aa1de79afcaeee24ffea18297d0af575
Matthew B Hartley <matthewbhartley@Matthews-MacBook-Air.local>
2014-01-21 22:10:04 -0500

    first four complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+//EXAMPLE 1//
+$('.activate-box').click(function(){
+	$('.box').addClass ('active');
+})
+//EXAMPLE 2//
+$('.hover-box').hover(function(){
+	$('.circle').addClass('hover');
+	$('.my-reply').addClass('hover');
+})
+//EXAMPLE 3//
+$('.click-hide').click(function(){
+	$('.tree-pic').fadeOut(1500);
+})
+//EXAMPLE 4//
+$('.click-to-choose').click(function(){
+	$('.truck-pic').addClass('active');
+})
